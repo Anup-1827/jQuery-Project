ddd227c702ebc0b66d9f594a1cb523ead862750c
Anup-1827 <anupalone.off@gmail.com>
2021-07-14 23:50:05 +0530

    04-NavBar

--- 04-NavBar/app.js
+++ 04-NavBar/app.js
@@ -1,16 +1,13 @@
-const hamburger = document.querySelector('.hamburger');
-const header =document.querySelector('.header');
-
-// hamburger.addEventListener('click', ()=>{
-//     if(header.classList.contains('show-list')){
-//         header.classList.remove('show-list');
-//     } else{
-//         header.classList.add('show-list');
+// $('.hamburger').on('click', ()=>{
+//     if($('.header').hasClass('show-list')){
+//         $('.header').removeClass('show-list');
+//     }else{
+//         $('.header').addClass('show-list')
 //     }
-
 // })
 
-
-hamburger.addEventListener('click',()=>{
-    header.classList.toggle('show-list');
+$('.hamburger').on('click', ()=>{
+    console.log('Anup')
+    $('.header').toggleClass('show-list');
 })
+
